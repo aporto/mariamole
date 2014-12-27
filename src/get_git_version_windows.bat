1258c87e9ee70d3f3ef82f67ca1ddaf2fa6076bb
aporto <alex.itajuba@gmail.com>
2014-12-27 08:32:03 -0200

    fixed error on get_git_version.bat

--- src/get_git_version_windows.bat
+++ src/get_git_version_windows.bat
@@ -9,7 +9,7 @@ echo #define STRINGIZE2(s) #s >> git_version.h
 echo #define STRINGIZE(s) STRINGIZE2(s) >> git_version.h
 echo // >> git_version.h
 set /p=#define GIT_VERSION_NUMBER >> git_version.h
-git describe --abbrev=1 >> git_version.h
+git describe --tags --abbrev=1 >> git_version.h
 echo #define GIT_VERSION_STRING STRINGIZE(GIT_VERSION_NUMBER) >> git_version.h
 echo #endif >> git_version.h
 
